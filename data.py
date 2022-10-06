6dc114fdfeba5c7949b41693eaee61724b2a50db
Zoul Aimi <mdzoul@gmail.com>
2022-10-06 14:24:39 +0800

    Initial commit

---
+++ data.py
@@ -0,0 +1,245 @@
+question_data = [
+	{
+		"category": "Science: Computers",
+		"type": "boolean",
+		"difficulty": "medium",
+		"question": "FLAC stands for 'Free Lossless Audio Condenser'.",
+		"correct_answer": "False",
+		"incorrect_answers": ["True"]
+	},
+	{
+		"category": "Entertainment: Music",
+		"type": "boolean",
+		"difficulty": "medium",
+		"question": "Rapper Snoop Dogg's real name is 'Cordozar Calvin Broadus, Jr.'.",
+		"correct_answer": "True",
+		"incorrect_answers": ["False"]
+	},
+	{
+		"category": "Entertainment: Music",
+		"type": "boolean",
+		"difficulty": "medium",
+		"question": "Soulja Boy's 'Crank That' won a Grammy for Best Rap Song in 2007.",
+		"correct_answer": "False",
+		"incorrect_answers": ["True"]
+	},
+	{
+		"category": "Politics",
+		"type": "boolean",
+		"difficulty": "easy",
+		"question": "There was a satirical candidate named 'Deez Nuts' running in the 2016 US presidential elections.",
+		"correct_answer": "True",
+		"incorrect_answers": ["False"]
+	},
+	{
+		"category": "Science & Nature",
+		"type": "boolean",
+		"difficulty": "medium",
+		"question": "A defibrillator is used to start up a heartbeat once a heart has stopped beating.",
+		"correct_answer": "True",
+		"incorrect_answers": ["False"]
+	},
+	{
+		"category": "Entertainment: Music",
+		"type": "boolean",
+		"difficulty": "medium",
+		"question": "The country song 'A Boy Named Sue' was written by Shel Silverstein.",
+		"correct_answer": "True",
+		"incorrect_answers": ["False"]
+	},
+	{
+		"category": "Entertainment: Books",
+		"type": "boolean",
+		"difficulty": "easy",
+		"question": "The book 1984 was published in 1949.",
+		"correct_answer": "True",
+		"incorrect_answers": ["False"]
+	},
+	{
+		"category": "History",
+		"type": "boolean",
+		"difficulty": "easy",
+		"question": "The United States Department of Homeland Security was formed in response to the September 11th attacks.",
+		"correct_answer": "True",
+		"incorrect_answers": ["False"]
+	},
+	{
+		"category": "General Knowledge",
+		"type": "boolean",
+		"difficulty": "medium",
+		"question": "An eggplant is a vegetable.",
+		"correct_answer": "False",
+		"incorrect_answers": ["True"]
+	},
+	{
+		"category": "Science: Computers",
+		"type": "boolean",
+		"difficulty": "easy",
+		"question": "Ada Lovelace is often considered the first computer programmer.", "correct_answer": "True",
+		"incorrect_answers": ["False"]
+	},
+	{
+		"category": "Entertainment: Cartoon & Animations",
+		"type": "boolean",
+		"difficulty": "easy",
+		"question": "Bill Cipher in the show 'Gravity Falls' is the good guy.",
+		"correct_answer": "False",
+		"incorrect_answers": ["True"]
+	},
+	{
+		"category": "General Knowledge",
+		"type": "boolean",
+		"difficulty": "easy",
+		"question": "In 2010, Twitter and the United States Library of Congress partnered together "
+		"to archive every tweet by American citizens.",
+		"correct_answer": "True",
+		"incorrect_answers": ["False"]
+	},
+	{
+		"category": "Entertainment: Comics",
+		"type": "boolean",
+		"difficulty": "easy",
+		"question": "Deadpool's identity is Slade Wilson.",
+		"correct_answer": "False",
+		"incorrect_answers": ["True"]
+	},
+	{
+		"category": "Geography",
+		"type": "boolean",
+		"difficulty": "easy",
+		"question": "There are no deserts in Europe.",
+		"correct_answer": "True",
+		"incorrect_answers": ["False"]
+	},
+	{
+		"category": "Entertainment: Books",
+		"type": "boolean",
+		"difficulty": "easy",
+		"question": "Stephen Chbosky wrote the book 'The Perks of Being A Wallflower'.",
+		"correct_answer": "True",
+		"incorrect_answers": ["False"]
+	},
+	{
+		"category": "Politics",
+		"type": "boolean",
+		"difficulty": "hard",
+		"question": "Joko Widodo has appeared in the cover of a TIME magazine.",
+		"correct_answer": "True",
+		"incorrect_answers": ["False"]
+	},
+	{
+		"category": "Entertainment: Video Games",
+		"type": "boolean",
+		"difficulty": "easy",
+		"question": "The Konami Code is known as Up, Up, Down, Down, Left, Right, Right, Left, B, A, Start.",
+		"correct_answer": "False",
+		"incorrect_answers": ["True"]
+	},
+	{
+		"category": "History",
+		"type": "boolean",
+		"difficulty": "easy",
+		"question": "Kublai Khan is the grandchild of Genghis Khan?",
+		"correct_answer": "True",
+		"incorrect_answers": ["False"]
+	},
+	{
+		"category": "Science & Nature",
+		"type": "boolean",
+		"difficulty": "easy",
+		"question": "An average human can go two weeks without water.",
+		"correct_answer": "False",
+		"incorrect_answers": ["True"]
+	},
+	{
+		"category": "Entertainment: Video Games",
+		"type": "boolean",
+		"difficulty": "easy",
+		"question": "'Metal Gear Solid 3: Snake Eater' was released in 2004.",
+		"correct_answer": "True",
+		"incorrect_answers": ["False"]
+	},
+	{
+		"category": "Politics",
+		"type": "boolean",
+		"difficulty": "hard",
+		"question": "George Clinton, Vice President of the United States (1805-1812), "
+		"is an ancestor of President Bill Clinton.",
+		"correct_answer": "False",
+		"incorrect_answers": ["True"]
+	},
+	{
+		"category": "Entertainment: Video Games",
+		"type": "boolean",
+		"difficulty": "medium",
+		"question": "In 'Call Of Duty: World At War', the first appearance of the 'PPSH-41' in 'Zombies' "
+		"was in the map 'Der Riese'.",
+		"correct_answer": "False",
+		"incorrect_answers": ["True"]
+	},
+	{
+		"category": "Entertainment: Film",
+		"type": "boolean",
+		"difficulty": "easy",
+		"question": "Actor Tommy Chong served prison time.",
+		"correct_answer": "True",
+		"incorrect_answers": ["False"]
+	},
+	{
+		"category": "General Knowledge",
+		"type": "boolean",
+		"difficulty": "easy",
+		"question": "The National Animal of Scotland is the Unicorn.",
+		"correct_answer": "True",
+		"incorrect_answers": ["False"]
+	},
+	{
+		"category": "Entertainment: Video Games",
+		"type": "boolean",
+		"difficulty": "medium",
+		"question": "The scrapped Sonic the Hedgehog 2 level 'Hidden Palace Zone' "
+		"was later reused in the iOS port of the game. ",
+		"correct_answer": "True",
+		"incorrect_answers": ["False"]
+	},
+	{
+		"category": "Entertainment: Film",
+		"type": "boolean",
+		"difficulty": "medium",
+		"question": "The movie 'Tron' received an Oscar nomination for Best Visual Effects.",
+		"correct_answer": "False",
+		"incorrect_answers": ["True"]
+	},
+	{
+		"category": "Science & Nature",
+		"type": "boolean",
+		"difficulty": "easy",
+		"question": "Water always boils at 100C, 212F, 373.15K, no matter where you are.",
+		"correct_answer": "False",
+		"incorrect_answers": ["True"]
+	},
+	{
+		"category": "Entertainment: Cartoon & Animations",
+		"type": "boolean",
+		"difficulty": "medium",
+		"question": "Blue Danube was one of the musical pieces featured in Disney's 1940's film Fantasia.",
+		"correct_answer": "False",
+		"incorrect_answers": ["True"]
+	},
+	{
+		"category": "Science: Computers",
+		"type": "boolean",
+		"difficulty": "hard",
+		"question": "The T-Mobile Sidekick smartphone is a re-branded version of the Danger Hiptop.",
+		"correct_answer": "True",
+		"incorrect_answers": ["False"]
+	},
+	{
+		"category": "General Knowledge",
+		"type": "boolean",
+		"difficulty": "easy",
+		"question": "The mitochondria is the powerhouse of the cell.",
+		"correct_answer": "True",
+		"incorrect_answers": ["False"]
+	}
+]
---
+++ question_model.py
@@ -0,0 +1,7 @@
+class Question:
+	
+	def __init__(self, category, q_text, q_answer):
+		self.category = category
+		self.text = q_text
+		self.answer = q_answer
+		
